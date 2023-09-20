abe54d3fdee66f9382fa4712058c8c8fc131e664
osvaldoec <osvaldo.estrada@wizeline.com>
2023-09-20 16:59:20 -0600

    test

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
+  appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
